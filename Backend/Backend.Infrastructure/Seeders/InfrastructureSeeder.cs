d821312fb535d7f61a5062ed8116fc9b235ff4d9
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2025-03-15 08:45:40 +0100

    Create InfrastructureSeeder.cs
    seeder for roles in role manger

---
+++ Backend/Backend.Infrastructure/Seeders/InfrastructureSeeder.cs
@@ -0,0 +1,19 @@
+// Backend.Infrastructure/Seeders/InfrastructureSeeder.cs
+using Microsoft.AspNetCore.Identity;
+namespace Backend.Infrastructure.Seeders;
+
+public static class InfrastructureSeeder
+{
+    public static async Task SeedRolesAsync(RoleManager<IdentityRole<Guid>> roleManager)
+    {
+        var roles = new[] { "Admin", "User" };
+        
+        foreach (var role in roles)
+        {
+            if (!await roleManager.RoleExistsAsync(role))
+            {
+                await roleManager.CreateAsync(new IdentityRole<Guid>(role));
+            }
+        }
+    }
+}
