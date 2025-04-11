bd46a7930c59f37a809fcf31aee0c8fe099b8b27
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2025-04-11 17:10:25 +0200

    Update IUserRepository.cs
    updated to reflect implementation

--- Backend/Backend.Application/Interfaces/IUserRepository.cs
+++ Backend/Backend.Application/Interfaces/IUserRepository.cs
@@ -1,12 +1,13 @@
 // Application/Interfaces/IUserRepository.cs
+using System.Linq.Expressions;
 using Backend.Domain.Entities;
+using FluentResults;
 
-namespace Backend.Application.Interfaces
+namespace Backend.Application.Interfaces;
+public interface IUserRepository
 {
-    public interface IUserRepository
-    {
-        Task<User?> GetByIdAsync(Guid id);
-        Task<User> AddAsync(User user, string password);
-        // Define other necessary methods (e.g., Update, Delete, etc.)
-    }
+    Task<Result<bool>> ExistsAsync(Expression<Func<User, bool>> predicate);
+    Task<Result<User>> FirstOrDefaultAsync(Expression<Func<User, bool>> predicate);
+    Task<Result<Guid>> AddAsync(User user);
+    Task<Result<User>> GetByIdAsync(Guid id);
 }
