c08804a09ba4ba48463a6e0d549a957fe40f0c27
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2025-04-11 17:13:24 +0200

    del unnecessary files

--- Backend/Backend.Application/DTOs/Auth/AuthenticationResult.cs
+++
@@ -1,9 +0,0 @@
-namespace Backend.Application.DTOs.Auth;
-
-public class AuthenticationResult // public record AuthenticationResult insted of class mby?
-{
-    public bool Succeeded { get; init; }
-    public bool RequiresTwoFactor { get; init; }
-    public bool IsLockedOut { get; init; }
-    public string? ErrorMessage { get; set; }
-}
