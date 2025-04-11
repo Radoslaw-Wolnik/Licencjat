ead11b9336679d9c8d0041f07609442afd06c3e4
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2025-04-11 17:10:07 +0200

    improved mappings

---
+++ Backend/Backend.Infrastructure/Mapping/AuthProfile.cs
@@ -0,0 +1,22 @@
+// Backend.Infrastructure/Mapping/AuthProfile.cs
+using Backend.Application.DTOs.Auth;
+using Backend.Application.Features.Auth;
+using AutoMapper;
+using Backend.Domain.Entities;
+
+namespace Backend.Application.Mapping;
+
+public class AuthProfile : Profile
+{
+    public AuthProfile()
+    {
+        CreateMap<RegisterRequest, RegisterCommand>();
+        CreateMap<LoginRequest, LoginCommand>();
+        CreateMap<User, LoginResponse>()
+            .ConstructUsing(src => new LoginResponse(
+                src.Id, 
+                src.Email, 
+                src.Username
+            ));
+    }
+}
--- Backend/Backend.Infrastructure/Mapping/UserProfile.cs
+++ Backend/Backend.Infrastructure/Mapping/UserProfile.cs
@@ -12,11 +12,12 @@ public class UserProfile : Profile
         CreateMap<ApplicationUser, User>()
             .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
             .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
-            .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.UserName))
+            .ForMember(dest => dest.Username, opt => opt.MapFrom(src => src.UserName))
             .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.FirstName))
             .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.LastName))
             .ForMember(dest => dest.BirthDate, opt => opt.MapFrom(src => src.BirthDate))
-            .ForMember(dest => dest.Address, opt => opt.MapFrom(src => src.Address))
+            // .ForMember(dest => dest.Address, opt => opt.MapFrom(src => src.Address))
             .ReverseMap(); // For bidirectional mapping
+        
     }
 }
