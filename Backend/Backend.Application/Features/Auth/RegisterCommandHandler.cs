4a21f0bd8e78a19a73da8367ea570f6b2df681d1
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2025-04-18 22:45:29 +0200

    Update RegisterCommandHandler.cs
    added prints (i will propably remove them later on)

--- Backend/Backend.Application/Features/Auth/RegisterCommandHandler.cs
+++ Backend/Backend.Application/Features/Auth/RegisterCommandHandler.cs
@@ -26,17 +26,27 @@ public sealed class RegisterCommandHandler : IRequestHandler<RegisterCommand, Re
         RegisterCommand command,
         CancellationToken cancellationToken)
     {
+
+        Console.WriteLine($"[Register command handler] Succesfully passed to the register command handler");
+        Console.WriteLine($"Email passed: {command.Email}");
+
         // 1. Check uniqueness via repository
         var emailExists = await _userRepo.ExistsAsync(u => u.Email == command.Email);
         if (emailExists.Value) return Result.Fail<Guid>(AuthErrors.EmailAlreadyExists);
 
+        Console.WriteLine($"[Register command handler] checked the email for colisions");
+
         var usernameExists = await _userRepo.ExistsAsync(u => u.Username == command.Username);
         if (usernameExists.Value) return Result.Fail<Guid>(AuthErrors.UsernameTaken);
+
+        Console.WriteLine($"[Register command handler] checked the username for colisions");
+        
 
         // 2. Create domain entity
 
         var code = CountryCode.Create(command.Country);
         if (code.IsFailed) return Result.Fail<Guid>(AuthErrors.InvalidCountryCode);
+        
 
         var loc = Location.Create(city: command.City, country: code.Value);
         if (loc.IsFailed) return Result.Fail<Guid>(LocationErrors.WrongLocation);
@@ -50,6 +60,7 @@ public sealed class RegisterCommandHandler : IRequestHandler<RegisterCommand, Re
             loc.Value);
         
         if (userResult.IsFailed) return Result.Fail<Guid>(userResult.Errors);
+        Console.WriteLine($"[Register command handler] Created domain entity");
 
         // 3. Create identity user (password handled here)
         var identityResult = await _identityService.CreateUserWithPasswordAsync(
@@ -64,9 +75,12 @@ public sealed class RegisterCommandHandler : IRequestHandler<RegisterCommand, Re
 
         if (identityResult.IsFailed)
             return Result.Fail<Guid>(identityResult.Errors);
+        
+        Console.WriteLine($"[Register command handler] Created identity service response success");
 
         // 4. Persist domain user
         await _userRepo.AddAsync(userResult.Value);
+        Console.WriteLine($"[Register command handler] repo added");
         return userResult.Value.Id;
     }
 }
