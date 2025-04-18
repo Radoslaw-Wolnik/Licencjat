388c7c64fe01151442218bc3bff00b16c47872f2
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2025-04-18 22:43:21 +0200

    Update IUserRepository.cs

--- Backend/Backend.Application/Interfaces/IUserRepository.cs
+++ Backend/Backend.Application/Interfaces/IUserRepository.cs
@@ -1,6 +1,7 @@
 // Application/Interfaces/IUserRepository.cs
 using System.Linq.Expressions;
 using Backend.Application.DTOs;
+using Backend.Application.DTOs.Auth;
 using Backend.Domain.Entities;
 using FluentResults;
 
@@ -8,7 +9,8 @@ namespace Backend.Application.Interfaces;
 public interface IUserRepository
 {
     Task<Result<bool>> ExistsAsync(Expression<Func<UserProjection, bool>> predicate);
-    Task<Result<User>> FirstOrDefaultAsync(Expression<Func<UserProjection, bool>> predicate);
+    Task<Result<User>> GetByAsync(Expression<Func<UserProjection, bool>> predicate);
     Task<Result<Guid>> AddAsync(User user);
     Task<Result<User>> GetByIdAsync(Guid id);
+    Task<Result<LoginUserInfo>> GetLoginInfoAsync(Expression<Func<UserProjection,bool>> predicate);
 }
