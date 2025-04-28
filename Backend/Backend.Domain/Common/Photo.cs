6902ed2ecefb4bb64d405b5f9fe7ed21a7dbaa29
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2025-04-28 15:05:19 +0200

    new common entites for better app logic

---
+++ Backend/Backend.Domain/Common/Photo.cs
@@ -0,0 +1,16 @@
+// Backend.Domain/Common/Photo.cs
+using FluentResults;
+
+namespace Backend.Domain.Common;
+
+public sealed record Photo(string Link)
+{
+    public string Link { get; } = Link;
+
+    public static Result<Photo> Create(string link)
+    {
+        // validate the link
+        
+        return new Photo(link);
+    }
+}
---
+++ Backend/Backend.Domain/Common/Rating.cs
@@ -0,0 +1,17 @@
+// Backend.Domain/Common/Rating.cs
+using FluentResults;
+
+namespace Backend.Domain.Common;
+
+public sealed record Rating(float Value)
+{
+    public static Rating Initial() => new(8.0f);
+
+    public static Result<Rating> Create(float value)
+    {
+        if (value < 1 || value > 10)
+            return Result.Fail("Rating must be between 1 and 10");
+
+        return new Rating(value);
+    }
+}
