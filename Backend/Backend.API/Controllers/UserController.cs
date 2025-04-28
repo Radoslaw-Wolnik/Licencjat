72b71bc8796cb6870d64e2a7fa59519a5642d1ed
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2025-04-28 14:57:40 +0200

    created general controllers

---
+++ Backend/Backend.API/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+
+
+// [Authorize(Policy = "IsAccountOwner")]
+// [HttpGet("me")]
+// public async Task<IActionResult> GetCurrentUserProfile() { ... }
+
+
+// getOwnSensitiveInfo
+
+// getUserProfile
+
+// get user Library paginated filtered
+
+// get user wishlist pagineted filtered
+
+// get user ...
+
+// get user followers/following/blocked
+
+
+// set ...
+// set profile picture
+// set user profile info things
+
+// update ...
+// add sth
+// change user profile picture
+// change user info things
+
+// delete ...
+// remove profile picture
+// remove sth
