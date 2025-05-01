116fe907a3afa8f9149fcf634791a095948f923b
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2025-05-01 16:18:15 +0200

    Update LocationCordinates.cs

--- Backend/Backend.Domain/Common/LocationCordinates.cs
+++ Backend/Backend.Domain/Common/LocationCordinates.cs
@@ -1,9 +1,11 @@
-// Backend.Domain/Common/Location.cs
 using FluentResults;
+
 namespace Backend.Domain.Common;
 
 public sealed record LocationCoordinates(double Latitude, double Longitude)
 {
+    public double Latitude { get; private set; } = Latitude;
+    public double Longitude { get; private set; } = Longitude;
     public static Result<LocationCoordinates> Create(double latitude, double longitude)
     {
         if (Math.Abs(latitude) > 90 || Math.Abs(longitude) > 180)
