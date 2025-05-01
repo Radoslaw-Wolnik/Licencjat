1ad17818d7ec73e2dc533ce05494f097297f880e
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2025-05-01 17:03:34 +0200

    added reconstituate and collection value objects

--- Backend/Backend.Domain/Entities/Swap.cs
+++ Backend/Backend.Domain/Entities/Swap.cs
@@ -1,44 +1,61 @@
-// Backend.Domain/Entities/Swap.cs
 using Backend.Domain.Common;
 using Backend.Domain.Enums;
 using Backend.Domain.Errors;
+using Backend.Domain.ValueObjects;
 using FluentResults;
 
 namespace Backend.Domain.Entities;
 
 public sealed class Swap : Entity<Guid>
 {
-    public Guid SubSwapRequestingId { get; }
-    public Guid? SubSwapAcceptingId { get; }
+    public SubSwap SubSwapRequesting { get; }
+    public SubSwap SubSwapAccepting { get; }
 
-    private readonly List<Guid> _meetups = new(); // only two are allowed - one at begging and one at the end
-    private readonly List<Guid> _timelineUpdates = new();
+    private readonly MeetupsCollection _meetups = new(); // only two are allowed - one at begging and one at the end
+    private readonly TimelineUpdatesCollection _timelineUpdates = new();
 
-    public IReadOnlyCollection<Guid> Meetups => _meetups.AsReadOnly();
-    public IReadOnlyCollection<Guid> TimelineUpdates => _timelineUpdates.AsReadOnly();
+    public IReadOnlyCollection<Meetup> Meetups => _meetups.Meetups;
+    public IReadOnlyCollection<TimelineUpdate> TimelineUpdates => _timelineUpdates.TimelineUpdates;
 
-    private Swap(Guid subSwapRequesitngId) // mby we actually need id in all entites as they are made based on the dbEntities? Although alternatively wehn we first make domain entity and then the dbentity we dont care about ids
+    private Swap(SubSwap subSwapRequesitng, SubSwap subSwapAccepting) // mby we actually need id in all entites as they are made based on the dbEntities? Although alternatively wehn we first make domain entity and then the dbentity we dont care about ids
     {
-        SubSwapRequestingId = subSwapRequesitngId;
+        SubSwapRequesting = subSwapRequesitng;
+        SubSwapAccepting = subSwapAccepting;
     }
 
-    public static Result<Swap> Create(Guid subSwapRequesitngId)
+    public static Result<Swap> Create(SubSwap subSwapRequesitng, SubSwap subSwapAccepting)
     {
         // if (subSwapAId == subSwapBId)
         //     return Result.Fail(SwapErrors.SameSubSwapError);
 
-        return Result.Ok(new Swap(subSwapRequesitngId));
+        return Result.Ok(new Swap(subSwapRequesitng, subSwapAccepting));
     }
 
-    public Result AddMeetup(Guid meetupId)
-    {
-        if (_meetups.Contains(meetupId))
-            return Result.Fail(SwapErrors.DuplicateMeetupError);
-        
-        if(_meetups.Count >= 2)
-            return Result.Fail(MeetupErrors.TooMany);
-            
-        _meetups.Add(meetupId);
-        return Result.Ok();
+    public static Swap Reconstitute(
+        SubSwap subSwapRequesitng,
+        SubSwap subSwapAccepting,
+        IEnumerable<Meetup>? meetups,
+        IEnumerable<TimelineUpdate>? timelineUpdates
+    ) {
+        var swap = new Swap(subSwapRequesitng, subSwapAccepting);
+        foreach(var m in meetups?? []) swap._meetups.Add(m);
+        foreach(var tu in timelineUpdates?? []) swap._timelineUpdates.Add(tu);
+
+        return swap;
     }
+
+    public Result AddMeetup(Meetup meetup)
+        => _meetups.Add(meetup);
+    
+    public Result RemoveMeetup(Guid meetupId)
+        => _meetups.Remove(meetupId);
+    
+    public Result UpdateMeetup(Meetup updatedMeetup)
+        => _meetups.Update(updatedMeetup);
+    
+    public Result AddTimelineUpdate(TimelineUpdate timelineUpdate)
+        => _timelineUpdates.Add(timelineUpdate);
+    
+    public Result RemoveTimelineUpdate(Guid timelineUpdateId)
+        => _timelineUpdates.Remove(timelineUpdateId);
 }
