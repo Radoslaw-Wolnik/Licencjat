7ae3bcf9ebce96b6812088e21745b437a2e0c877
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2025-05-06 14:30:16 +0200

    added services and repositories

--- Backend/Backend.API/Program.cs
+++ Backend/Backend.API/Program.cs
@@ -22,6 +22,11 @@ using FluentValidation.AspNetCore;
 using FluentValidation;
 using Backend.Infrastructure.Repositories.UserBooks;
 using Backend.Infrastructure.Repositories.GeneralBooks;
+using Backend.Application.Interfaces.Repositories;
+using Backend.Infrastructure.Repositories.Swaps;
+using Backend.Infrastructure.Repositories.Users;
+using Backend.Application.Interfaces.DbReads;
+using Backend.Infrastructure.Services.DbReads;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -57,27 +62,60 @@ builder.Services.AddIdentity<UserEntity, IdentityRole<Guid>>(options =>
     .AddDefaultTokenProviders();
 
 // ========== INFRASTRUCTURE SERVICES ========== //
-// repositories
-// builder.Services.AddScoped<IGeneralBookRepository, GeneralBookRepository>();
-// builder.Services.AddScoped<ISwapRepository, SwapRepository>();
-// builder.Services.AddScoped<IUserBookRepository, UserBookRepository>();
-// im not sure but mby i should add subrepositories as well (the ones that are inherited by the main ones)
-builder.Services.AddScoped<IUserRepository, UserRepository>();
 
-builder.Services.AddScoped<DatabaseHealthCheck>();
+// repositories
+// general book
+builder.Services.AddScoped<IGeneralBookReviewsRepository, GeneralBookReviewsRepository>();
+builder.Services.AddScoped<IWriteGeneralBookRepository, WriteGeneralBookRepository>();
+
+// swap
+builder.Services.AddScoped<ISwapFeedbackRepository, SwapFeedbackRepository>();
+builder.Services.AddScoped<ISwapIssueRepository, SwapIssueRepository>();
+builder.Services.AddScoped<ISwapMeetupRepository, SwapMeetupRepository>();
+builder.Services.AddScoped<ISwapTimelineRepository, SwapTimelineRepository>();
+builder.Services.AddScoped<IWriteSwapRepository, WriteSwapRepository>();
+
+// user book
+builder.Services.AddScoped<IUserBookBookmarkRepository, UserBookBookmarkRepository>();
+builder.Services.AddScoped<IWriteUserBookRepository, WriteUserBookRepository>();
+
+// user
+builder.Services.AddScoped<IAuthUserRepository, AuthUserRepository>();
+builder.Services.AddScoped<IUserBlockedRepository, UserBlockedRepository>();
+builder.Services.AddScoped<IUserFollowingRepository, UserFollowingRepository>();
+builder.Services.AddScoped<IUserSocialMediaRepository, UserSocialMediaRepository>();
+builder.Services.AddScoped<IUserWishlistRepository, UserWishlistRepository>();
+builder.Services.AddScoped<IWriteUserRepository, WriteUserRepository>();
 
 // services
 builder.Services.AddScoped<ISignInService, SignInService>();
 builder.Services.AddScoped<IIdentityService, IdentityService>();
 builder.Services.AddTransient<IEmailService, EmailService>();
+builder.Services.AddTransient<IImageStorageService, MinioImageStorageService>();
+
+// read services
+builder.Services.AddTransient<IGeneralBookReadService, GeneralBookReadService>();
+builder.Services.AddTransient<ISwapReadService, SwapReadService>();
+builder.Services.AddTransient<IUserBookReadService, UserBookReadService>();
+builder.Services.AddTransient<IUserReadService, UserReadService>();
 
 // mappers
 builder.Services.AddAutoMapper(typeof(AuthProfile));
 builder.Services.AddAutoMapper(typeof(BookmarkProfile));
+builder.Services.AddAutoMapper(typeof(FeedbackProfile));
 builder.Services.AddAutoMapper(typeof(GeneralBookProfile));
+builder.Services.AddAutoMapper(typeof(IssueProfile));
+builder.Services.AddAutoMapper(typeof(MeetupProfile));
+builder.Services.AddAutoMapper(typeof(ReviewProfile));
 builder.Services.AddAutoMapper(typeof(SocialMediaProfile));
+builder.Services.AddAutoMapper(typeof(SubSwapProfile));
+builder.Services.AddAutoMapper(typeof(SwapProfile));
+builder.Services.AddAutoMapper(typeof(TimelineProfile));
 builder.Services.AddAutoMapper(typeof(UserBookProfile));
 builder.Services.AddAutoMapper(typeof(UserProfile)); // cfg => {cfg.AddExpressionMapping();},
+
+// health check for db
+builder.Services.AddScoped<DatabaseHealthCheck>();
 
 
 // ========== MEDIATR & VALIDATION ========== //
@@ -93,11 +131,15 @@ builder.Services.AddValidatorsFromAssemblyContaining<RegisterRequestValidator>()
 // Optional command validation pipeline
 builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
 
-// AutoMapper
+// AutoMapper - expression mapping added to specyfic mappers
 builder.Services.AddAutoMapper(cfg => 
 {
     cfg.AddExpressionMapping();
-    cfg.AddMaps(typeof(UserProfile), typeof(AuthProfile));
+    cfg.AddMaps(
+        typeof(UserProfile), 
+        typeof(AuthProfile),
+        typeof(GeneralBookProfile),
+        typeof(UserBookProfile));
 });
 
 // ========== SECURITY CONFIGURATION ========== //
