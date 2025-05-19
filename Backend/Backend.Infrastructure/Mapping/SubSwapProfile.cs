8659501fd5a67545ce0283573cebe5841322c835
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2025-05-19 12:23:36 +0100

    Update SubSwapProfile.cs
    fixed the subswap mepper to use Included sub properties correctly (UserBook, Issue, Feedback)

--- Backend/Backend.Infrastructure/Mapping/SubSwapProfile.cs
+++ Backend/Backend.Infrastructure/Mapping/SubSwapProfile.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using Backend.Domain.Common;
+using Backend.Domain.Entities;
 using Backend.Infrastructure.Entities;
 
 namespace Backend.Infrastructure.Mapping;
@@ -16,9 +17,15 @@ public class SubSwapProfile : Profile
                     src.Id,
                     src.UserId,
                     src.PageAt,
-                    src.UserBookReadingId,
-                    src.FeedbackId,
-                    src.IssueId
+                    src.UserBookReading != null 
+                        ? ctx.Mapper.Map<UserBook>(src.UserBookReading) 
+                        : null,
+                    src.Feedback != null 
+                        ? ctx.Mapper.Map<Feedback>(src.Feedback) 
+                        : null,
+                    src.Issue != null 
+                        ? ctx.Mapper.Map<Issue>(src.Issue) 
+                        : null
                 );
                 if (result.IsFailed)
                     throw new AutoMapperMappingException(
@@ -32,8 +39,8 @@ public class SubSwapProfile : Profile
             .ForMember(dest => dest.Id,                 opt => opt.MapFrom(src => src.Id))
             .ForMember(dest => dest.UserId,             opt => opt.MapFrom(src => src.UserId))
             .ForMember(dest => dest.PageAt,             opt => opt.MapFrom(src => src.PageAt))
-            .ForMember(dest => dest.UserBookReadingId,  opt => opt.MapFrom(src => src.UserBookReadingId))
-            .ForMember(dest => dest.FeedbackId,         opt => opt.MapFrom(src => src.FeedbackId))
-            .ForMember(dest => dest.IssueId,            opt => opt.MapFrom(src => src.IssueId));
+            .ForMember(dest => dest.UserBookReadingId,  opt => opt.MapFrom(src => src.UserBookReading != null ? (Guid?) src.UserBookReading.OwnerId : null))
+            .ForMember(dest => dest.FeedbackId,         opt => opt.MapFrom(src => src.Feedback != null ? (Guid?) src.Feedback.Id : null))
+            .ForMember(dest => dest.IssueId,            opt => opt.MapFrom(src => src.Issue != null ? (Guid?) src.Issue.Id : null));
     }
 }
