63a925c1e00eda03339dd4f054f03deb70bde602
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2025-05-19 12:26:26 +0100

    Update GenresCollection.cs
    added function to replace all genres - compering them to the previous ones

--- Backend/Backend.Domain/ValueObjects/GenresCollection.cs
+++ Backend/Backend.Domain/ValueObjects/GenresCollection.cs
@@ -34,5 +34,26 @@ public class GenresCollection
         _genres.Remove(genre);
         return Result.Ok();
     }
+
+    public Result Replace(IEnumerable<BookGenre> newGenres)
+    {
+        // _genres.Clear();
+        // if (newGenres != null)
+        // {
+        //    foreach (var g in newGenres.Distinct())
+        //        _genres.Add(g);
+        // }
+
+        newGenres = newGenres?.Distinct() ?? Enumerable.Empty<BookGenre>();
+
+        // Remove ones that are no longer present
+        foreach (var old in _genres.Except(newGenres).ToList())
+            _genres.Remove(old);
+
+        // Add the new ones that weren’t already there
+        foreach (var nxt in newGenres.Except(_genres))
+            _genres.Add(nxt);
+        return Result.Ok();
+    }
     
 }
